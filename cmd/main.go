b6bce7b4b1e64b966ce3421edffa4bc779ced270
Danila Tezin <danilatezin@gmail.com>
2022-11-24 17:54:05 +0500

    init

---
+++ cmd/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+
+	gotodo "github.com/DanilTezin/go-todo"
+	"github.com/DanilTezin/go-todo/pkg/handler"
+	"github.com/DanilTezin/go-todo/pkg/repository"
+	"github.com/DanilTezin/go-todo/pkg/service"
+	"github.com/spf13/viper"
+)
+
+func main() {
+
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(gotodo.Server)
+	if err := srv.Run("5000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("Error occured while running http server: %s", err.Error())
+	}
+
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
